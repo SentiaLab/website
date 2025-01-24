0fde5db1b1f5668b646e27824f431c18ab1265f1
Caio Piccirillo <caiopiccirillo@gmail.com>
2025-01-24 17:34:07 +0000

    chore: Recreate project

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-
-  images: {
-    dangerouslyAllowSVG: true,
-  },
-
-  reactStrictMode: true,
-
-  basePath: "/website",
 };
 
 export default nextConfig;
